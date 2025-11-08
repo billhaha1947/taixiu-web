c30a54309990a7f44db06954741be34a3cc07cf5
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-09 05:57:07 +0700

    Update firebaseAdmin.js

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,10 +1,8 @@
-import admin from "firebase-admin";
+const admin = require("firebase-admin");
 
 try {
-  // Lấy dữ liệu key từ biến môi trường
   const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
-  // Kiểm tra xem Firebase đã khởi tạo chưa
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
@@ -12,10 +10,9 @@ try {
     console.log("✅ Firebase Admin initialized successfully");
   }
 
-  // Xuất Firestore để dùng ở các file khác
   const db = admin.firestore();
-  export { db };
-  
+  module.exports = { db };
+
 } catch (error) {
   console.error("❌ Firebase Admin initialization failed:", error);
   throw new Error("Firebase initialization error");
