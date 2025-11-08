50ac8d7ec446f8ba3acfa633a864fbb9f60798c7
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-11-08 22:18:07 +0700

    Update firebaseAdmin.js

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,1 +1,18 @@
+// server/firebaseAdmin.js
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// Dữ liệu private key của Firebase (copy từ Firebase Service Account)
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
+
+export const db = admin.firestore();
+export const auth = admin.auth();
